b7320fbbe2ab3acdbb7fec89f1af6e4f4a2d1ad3
ZsoltNagy91 <zsolt.j.nagy@gmail.com>
2026-09-23 19:42:00 +0200

    Scaffold a controller

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TodoContext") ?? throw new InvalidOperationException("Connection string 'TodoContext' not found.");
 
 // Add services to the container.
 
--- obj/Debug/net10.0/TodoApi_2.AssemblyInfo.cs
+++ obj/Debug/net10.0/TodoApi_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12606188daa3069fa05b5d97626c9f22a693161c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91dad2246801a86ecdd074b7197e390f38469ed3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
