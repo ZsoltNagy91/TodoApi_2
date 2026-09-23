95984a01007c2ae1c796b7628b7bb37f543d14af
ZsoltNagy91 <zsolt.j.nagy@gmail.com>
2026-09-23 19:32:57 +0200

    Add a database context

--- obj/Debug/net10.0/TodoApi_2.AssemblyInfo.cs
+++ obj/Debug/net10.0/TodoApi_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95103ff0f5fe69f915542a9403976cb80862a4f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12606188daa3069fa05b5d97626c9f22a693161c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
